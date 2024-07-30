830a8045c30b3fe2df7404ef50184d5eaf2edd39
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-07-30 16:59:07 +0300

    feat: window.py realized game cycle feat: server.py, client.py adapted to game app

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
-from classes import Game
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from window import GameWindow
 
 
-g = Game()
-g.create_connection("localhost", 9999)
+if __name__ == "__main__":
+    
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = GameWindow()
+    MainWindow.show()
+    MainWindow.create_connection("localhost", 9999)
+    MainWindow.thread.start()
+    sys.exit(app.exec_())
--- window.py
+++ window.py
@@ -1,56 +1,130 @@
-import sys
+import socket
 import json
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from classes import Game
 
 with open("constants.json", mode = 'r', encoding = "utf-8") as f:
     const = json.load(f)
 
+
+class GameWindow(QtWidgets.QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.field: list[list[QtWidgets.QPushButton]] = [[], [], []]
+        self.winner: str = None
+        self.you: str = None
+        self.opponent: str = None
+        self.turn: str = 'X'
+        self.moves_cnt: int = 0
+
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
+
+        self.client = None
+    
+    def create_connection(self, host, port):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((host, port))
+        server.listen(1)
+
+        client, adress = server.accept()
+
+        self.client = client
+        self.you = 'X'
+        self.opponent = 'O'
+
+        self.thread = GameWindowThread(self)
+
+    def connect(self, host, port):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, port))
+
+        self.client = client
+        self.you = 'O'
+        self.opponent = 'X'
+
+        self.thread = GameWindowThread(self)
+
+    def move(self, x: int, y: int):
+        if self.field[x][y].text() == '':
+            self.field[x][y].setText(self.turn)
+            self.moves_cnt += 1
+            self.turn = self.opponent if self.turn == self.you else self.you
+
+    def cell_slot(self, x: int, y: int):
+        if self.turn == self.you:
+            self.move(x, y)
+            m = f"{x} {y}"
+            self.client.send(m.encode("utf-8"))
+    
+    def check_win(self):
+        for row in self.field:
+            if row[0].text() == row[1].text() == row[2].text() != '':
+                return self.opponent
+        for col in range(3):
+            if self.field[0][col].text() == self.field[1][col].text() == self.field[2][col].text() != '':
+                return self.opponent
+        if self.field[0][0].text() == self.field[1][1].text() == self.field[2][2].text() != '':
+            return self.opponent
+        if self.field[0][2].text() == self.field[1][1].text() == self.field[2][0].text() != '':
+            return self.opponent
+        if self.moves_cnt == 9:
+            return "Tie"
+        return None
+
+
+class GameWindowThread(QtCore.QThread):
+    def __init__(self, main_window: GameWindow) -> None:
+        super().__init__()
+        self.main_window = main_window
+
+    def run(self):
+        while self.main_window.winner is None:
+            if self.main_window.turn != self.main_window.you:
+                move = self.main_window.client.recv(3)
+                if move:
+                    self.main_window.move(*list(map(int, move.decode("utf-8").split())))
+            self.main_window.winner = self.main_window.check_win()
+        match(self.main_window.winner):
+            case self.main_window.you:
+                print("You win!")
+            case "Tie":
+                print("Tie")
+            case _:
+                print("You lose!")
+        self.main_window.client.close()
+
+
 class Ui_MainWindow(object):
 
-    def setupUi(self, MainWindow: QtWidgets.QMainWindow):
-
-        self.game = Game()
+    def setupUi(self, MainWindow: GameWindow):
 
         MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowTitle("TicTacToe")
         MainWindow.resize(800, 600)
 
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.centralwidget = QtWidgets.QWidget(MainWindow)
+        MainWindow.centralwidget.setObjectName("centralwidget")
 
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(140, 80, const["CELL_SIZE"]*3, const["CELL_SIZE"]*3))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(spacing = 0)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setObjectName("gridLayout")
+        MainWindow.gridLayoutWidget = QtWidgets.QWidget(MainWindow.centralwidget)
+        MainWindow.gridLayoutWidget.setGeometry(QtCore.QRect(140, 80, const["CELL_SIZE"]*3, const["CELL_SIZE"]*3))
+        MainWindow.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        MainWindow.gridLayout = QtWidgets.QGridLayout(spacing = 0)
+        MainWindow.gridLayout.setContentsMargins(0, 0, 0, 0)
+        MainWindow.gridLayout.setObjectName("gridLayout")
 
-        self.gridLayoutWidget.setLayout(self.gridLayout)
+        MainWindow.gridLayoutWidget.setLayout(MainWindow.gridLayout)
 
-        self.field: list[QtWidgets.QPushButton] = []
+        for i in range(3):
+            for j in range(3):
+                btn = QtWidgets.QPushButton(MainWindow.gridLayoutWidget)
+                MainWindow.field[i].append(btn)
+                btn.setObjectName("pushButton")
+                MainWindow.gridLayout.addWidget(btn, i, j, 1, 1)
+                btn.setFixedSize(const["CELL_SIZE"], const["CELL_SIZE"])
+                btn.clicked.connect(lambda state, x = i, y = j: MainWindow.cell_slot(x, y))
 
-        for i in range(9):
-            btn = QtWidgets.QPushButton(self.gridLayoutWidget)
-            self.field.append(btn)
-            btn.setObjectName("pushButton")
-            self.gridLayout.addWidget(btn, i//3, i%3, 1, 1)
-            btn.setFixedSize(const["CELL_SIZE"], const["CELL_SIZE"])
+        MainWindow.setCentralWidget(MainWindow.centralwidget)
 
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(230, 40, 47, 14))
-        self.label.setObjectName("label")
-
-        MainWindow.setCentralWidget(self.centralwidget)
-
-
-if __name__ == "__main__":
-    
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
