841cba2f675654895d3b04b5071c48c33b14f9c8
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-07-30 18:48:10 +0300

    feat: set styles for app refactor: changed thread class name

--- window.py
+++ window.py
@@ -35,7 +35,7 @@ class GameWindow(QtWidgets.QMainWindow):
         self.you = 'X'
         self.opponent = 'O'
 
-        self.thread = GameWindowThread(self)
+        self.thread = GameCycleThread(self)
 
     def connect(self, host, port):
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,7 +45,7 @@ class GameWindow(QtWidgets.QMainWindow):
         self.you = 'O'
         self.opponent = 'X'
 
-        self.thread = GameWindowThread(self)
+        self.thread = GameCycleThread(self)
 
     def move(self, x: int, y: int):
         if self.field[x][y].text() == '':
@@ -54,28 +54,29 @@ class GameWindow(QtWidgets.QMainWindow):
             self.turn = self.opponent if self.turn == self.you else self.you
 
     def cell_slot(self, x: int, y: int):
-        if self.turn == self.you:
+        if self.turn == self.you and not self.winner:
             self.move(x, y)
             m = f"{x} {y}"
             self.client.send(m.encode("utf-8"))
     
     def check_win(self):
+        prev_move: str = self.you if self.turn == self.opponent else self.opponent
         for row in self.field:
             if row[0].text() == row[1].text() == row[2].text() != '':
-                return self.opponent
+                return prev_move
         for col in range(3):
             if self.field[0][col].text() == self.field[1][col].text() == self.field[2][col].text() != '':
-                return self.opponent
+                return prev_move
         if self.field[0][0].text() == self.field[1][1].text() == self.field[2][2].text() != '':
-            return self.opponent
+            return prev_move
         if self.field[0][2].text() == self.field[1][1].text() == self.field[2][0].text() != '':
-            return self.opponent
+            return prev_move
         if self.moves_cnt == 9:
             return "Tie"
         return None
 
 
-class GameWindowThread(QtCore.QThread):
+class GameCycleThread(QtCore.QThread):
     def __init__(self, main_window: GameWindow) -> None:
         super().__init__()
         self.main_window = main_window
@@ -94,7 +95,6 @@ class GameWindowThread(QtCore.QThread):
                 print("Tie")
             case _:
                 print("You lose!")
-        self.main_window.client.close()
 
 
 class Ui_MainWindow(object):
@@ -107,11 +107,19 @@ class Ui_MainWindow(object):
 
         MainWindow.centralwidget = QtWidgets.QWidget(MainWindow)
         MainWindow.centralwidget.setObjectName("centralwidget")
+        MainWindow.centralwidget.setStyleSheet("background: #B28B5C")
 
         MainWindow.gridLayoutWidget = QtWidgets.QWidget(MainWindow.centralwidget)
         MainWindow.gridLayoutWidget.setGeometry(QtCore.QRect(140, 80, const["CELL_SIZE"]*3, const["CELL_SIZE"]*3))
         MainWindow.gridLayoutWidget.setObjectName("gridLayoutWidget")
         MainWindow.gridLayout = QtWidgets.QGridLayout(spacing = 0)
+        MainWindow.gridLayoutWidget.setStyleSheet("border: 4px solid rgba(255, 255, 255, 0.1);")
+
+        shadow = QtWidgets.QGraphicsDropShadowEffect()
+        shadow.setBlurRadius(15)
+        MainWindow.gridLayoutWidget.setGraphicsEffect(shadow)
+
+
         MainWindow.gridLayout.setContentsMargins(0, 0, 0, 0)
         MainWindow.gridLayout.setObjectName("gridLayout")
 
@@ -125,6 +133,7 @@ class Ui_MainWindow(object):
                 MainWindow.gridLayout.addWidget(btn, i, j, 1, 1)
                 btn.setFixedSize(const["CELL_SIZE"], const["CELL_SIZE"])
                 btn.clicked.connect(lambda state, x = i, y = j: MainWindow.cell_slot(x, y))
+                btn.setStyleSheet("background: rgba(255, 255, 255, 0.1)")
 
         MainWindow.setCentralWidget(MainWindow.centralwidget)
 
